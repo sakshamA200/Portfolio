cde080d24ffd1008107a9a185b073dca99807b61
sakshamA200 <saksham.zalpuri200@gmail.com>
2025-06-24 04:02:33 +0530

    Added some responsiveness

--- script/main.js
+++ script/main.js
@@ -1,8 +1,3 @@
- const toggle = document.getElementById('style-toggle-btn');
-
-  toggle.addEventListener('click', () => {
-    document.body.classList.toggle('dark_mode');
-  });
 const hamburger = document.getElementById('hamburger');
 const navLinks = document.querySelector('.nav-links');
 
